c1cbdb212e6bfd40c20c21aa401fc847c2b29cd2
aearmasaq <63509650+aearmasaq@users.noreply.github.com>
2020-06-27 14:04:33 -0500

    se agrego id en el html del reto 2 de la semana 13

--- js/reto2.js
+++ js/reto2.js
@@ -1,3 +1,15 @@
+function readFormData() {
+    let formData = {};
+    formData.nombre = document.getElementById("nombre").value;
+    formData.apellido = document.getElementById("apellido").value;
+    formData.raza = document.getElementById("raza").value;
+    formData.telefono = document.getElementById("telefono").value;
+    formData.pais = document.getElementById("pais").value;
+    formData.img = document.getElementById("img").value;
+    formData.descripcion = document.getElementById("descripcion").value;
+    return formData; 
+}
+
 let modal = document.querySelector(".modal");
 var trigger = document.querySelector(".buton__modal");
 var closeButton = document.querySelector(".close-button");
@@ -15,3 +27,8 @@ function windowOnClick(event) {
 trigger.addEventListener("click", toggleModal);
 closeButton.addEventListener("click", toggleModal);
 window.addEventListener("click", windowOnClick);
+
+
+
+
+
